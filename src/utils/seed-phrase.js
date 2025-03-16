156e054a7e9c88eb872acaaaa0e7223d9c0a9992
The SunShining <thesunshiningmin@gmail.com>
2025-03-16 17:05:22 -0600

    /// deleete seed phrese

--- src/utils/seed-phrase.js
+++
@@ -1,4 +0,0 @@
-const { parseSeedPhrase, generateSeedPhrase } = require('near-seed-phrase');
-
-// to create a seed phrase with its corresponding Keys
-const {seedPhrase, publicKey, secretKey} = generateSeedPhrase()
