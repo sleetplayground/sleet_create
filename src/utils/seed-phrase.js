0671e56ad22d6ec42c6e609988f077b63c0c2a5f
The SunShining <thesunshiningmin@gmail.com>
2025-03-16 16:52:40 -0600

    Update seed-phrase.js

--- src/utils/seed-phrase.js
+++ src/utils/seed-phrase.js
@@ -1,26 +1,4 @@
-import { generateSeedPhrase, parseSeedPhrase } from 'near-seed-phrase';
+const { parseSeedPhrase, generateSeedPhrase } = require('near-seed-phrase');
 
-export const generateNewSeedPhrase = () => {
-  const { seedPhrase, publicKey, secretKey } = generateSeedPhrase();
-  return {
-    seedPhrase,
-    publicKey,
-    privateKey: secretKey
-  };
-};
-
-export const recoverKeysFromSeedPhrase = (seedPhrase) => {
-  if (!seedPhrase) {
-    throw new Error('Seed phrase is required');
-  }
-
-  try {
-    const { publicKey, secretKey } = parseSeedPhrase(seedPhrase);
-    return {
-      publicKey,
-      privateKey: secretKey
-    };
-  } catch {
-    throw new Error('Invalid seed phrase');
-  }
-};
+// to create a seed phrase with its corresponding Keys
+const {seedPhrase, publicKey, secretKey} = generateSeedPhrase()
