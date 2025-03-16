4cabb333f9802f3de616c0e4d5f02ed1b939bc6b
The SunShining <thesunshiningmin@gmail.com>
2025-03-16 16:40:08 -0600

    // restt config

--- src/config.js
+++ src/config.js
@@ -3,22 +3,9 @@ const contractPerNetwork = {
   testnet: 'testnet',
 };
 
-// Chains for EVM Wallets
-const evmWalletChains = {
-  mainnet: {
-    chainId: 397,
-    name: 'Near Mainnet',
-    explorer: 'https://eth-explorer.near.org',
-    rpc: 'https://eth-rpc.mainnet.near.org',
-  },
-  testnet: {
-    chainId: 398,
-    name: 'Near Testnet',
-    explorer: 'https://eth-explorer-testnet.near.org',
-    rpc: 'https://eth-rpc.testnet.near.org',
-  },
-};
 
-export const NetworkId = 'testnet';
-export const HelloNearContract = contractPerNetwork[NetworkId];
-export const EVMWalletChain = evmWalletChains[NetworkId];
+const networkId = localStorage.getItem('networkId') || 'testnet';
+export const NetworkId = networkId;
+
+
+export const NearContract = contractPerNetwork[NetworkId];
