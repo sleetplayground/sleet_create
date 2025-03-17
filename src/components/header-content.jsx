45cc7525e3316ee1b3a229a31d5a310bf4a38265
The SunShining <thesunshiningmin@gmail.com>
2025-03-16 20:14:29 -0600

    header

--- src/components/header-content.jsx
+++ src/components/header-content.jsx
@@ -1,8 +1,10 @@
 import styles from '@/styles/header-content.module.css';
+import sleetIcon from '@/assets/sleet_code_icon_trans.png';
 
 export const HeaderContent = () => {
   return (
     <div className={styles.container}>
+      <img src={sleetIcon} alt="Sleet Code Icon" className={styles.logo} />
       <h1 className={styles.title}>SLEET<br/>CREATE</h1>
       <p className={styles.subtitle}>NEAR ACCOUNT CREATION<br/>AND LEARNING PLAYGROUND</p>
     </div>
