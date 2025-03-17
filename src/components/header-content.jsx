00c03478a33c10fe059f99f03ecafdca9c7a8abf
The SunShining <thesunshiningmin@gmail.com>
2025-03-16 19:38:28 -0600

    Update header-content.jsx

--- src/components/header-content.jsx
+++ src/components/header-content.jsx
@@ -3,7 +3,7 @@ import styles from '@/styles/header-content.module.css';
 export const HeaderContent = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>SLEET ACCOUNT</h1>
+      <h1 className={styles.title}>SLEET CREATE</h1>
       <p className={styles.subtitle}>NEAR ACCOUNT CREATION PLAYGROUND</p>
       <div className={styles.notice}>
         <p>NOTE: THE RECOMMEND WAY FOR USERS TO CREATE NEW NEAR ACCOUNTS IS WITH A NEAR WALLET APP LIKE METEOR WALLET OR BITTE. ONLY USE THIS TOOL IF YOU KNOW WHAT YOU ARE DOING!</p>
